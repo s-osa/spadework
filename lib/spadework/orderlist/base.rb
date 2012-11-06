e81edc98545ac422d349852190f7b27484daef7b
Shunsuke Osa <hhelibebcnofnenamg@gmail.com>
2012-11-06 17:56:31 +0900

    refactor OrderList::Base, OrderList::Rakuten.

--- lib/spadework/orderlist/base.rb
+++ lib/spadework/orderlist/base.rb
@@ -3,4 +3,8 @@
 require 'csv'
 
 class OrderList::Base < Array
+  attr_accessor :header, :orders  
+  def size
+    self.orders.size + 1
+  end
 end
--- lib/spadework/orderlist/rakuten.rb
+++ lib/spadework/orderlist/rakuten.rb
@@ -2,21 +2,8 @@
 
 class OrderList::Rakuten < OrderList::Base
   def initialize(path)
-    @arr ||= []
-    CSV.foreach(path,"r:windows-31j") do |row|
-      @arr << row.map { |col| col.to_s.encode("utf-8") }
-    end
-  end
-
-  def size
-    @arr.size
-  end
-
-  def header
-    @arr[0]
-  end
-
-  def orders
-    @arr[1..-1]
+    reader = CSV.open(path, "r:windows-31j")
+    @header = reader.shift.map { |col| col.to_s.encode("utf-8") }
+    @orders = reader.map { |row| Order::Rakuten.new(row.map { |col| col.to_s.encode("utf-8") } ) }
   end
 end
